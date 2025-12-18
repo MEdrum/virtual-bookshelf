47ebacdde410f0435b32d9c4e735e68c8488447c
Ainex <xenia.neumann@protonmail.com>
2025-12-18 13:44:58 +0100

    add data model for bookshelf

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,3 +1,35 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    bookID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    isbn = models.CharField(max_length=17, unique=True) #13 characters with 4 hyphens
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+    year_of_publication = models.IntegerField(blank=True, null=True)
+    visibility = {
+        "0": "private",
+        "1": "friends",
+        "2": "public",
+    }
+    borrowable = models.BooleanField(default=True)
+    ownerID = models.ForeignKey('home.User', on_delete=models.CASCADE)
+    shelfID = models.ForeignKey('Shelf', on_delete=models.CASCADE)
+    coverURL = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Shelf(models.Model):
+    shelfID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    ownerID = models.ForeignKey('home.User', on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
