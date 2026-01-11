d103f7331538f74c8f32312d0efab9aafc16ebcf
Ainex <xenia.neumann@protonmail.com>
2026-01-11 22:20:27 +0100

    add password change and reset

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
 
--- virtual_bookshelf/settings.py
+++ virtual_bookshelf/settings.py
@@ -143,3 +143,4 @@ MEDIA_URL = "media/"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
