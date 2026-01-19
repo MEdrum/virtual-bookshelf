add7314d79dc9edc4db1ea42b736bc033be94be9
Ainex <xenia.neumann@protonmail.com>
2026-01-19 01:27:10 +0100

    Add functionality for adding and deleting books. Fixes #3, #4

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -4,4 +4,6 @@ from bookshelf import views
 urlpatterns = [
     path("", views.bookshelves_overview, name="bookshelves"),
     path("<str:shelfid>/<str:isbn>/", views.view_book, name="view-book"),
-]
+    path('book/new/<str:shelfid>/', views.process_book_form, name='new_book'),
+    path('book/edit/<str:shelfid>/<uuid:bookid>/', views.process_book_form, name='edit_book'),
+   ]
